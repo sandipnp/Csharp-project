e413ffaf62e8da1f1d81a90b7d40f70aeb57c93e
unknown <sandippandeynp@gmail.com>
2024-09-29 17:09:01 +0300

    adding carspeed limit project

---
+++ carspeedlimit/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+/*using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;*/
+
+namespace carspeedlimit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the speed limit ");
+            int SpeedLimit = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the speed of car");
+            int CarSpeed = int.Parse(Console.ReadLine());
+            if (CarSpeed <= SpeedLimit) {
+                Console.WriteLine("Your Speed is Ok");
+                    
+                    }
+            else {
+                Console.WriteLine("Your Speed over the limit");
+                int DemeritPoints = (CarSpeed - SpeedLimit) / 5;
+                Console.WriteLine("Demerit points:" +DemeritPoints);
+                if (DemeritPoints > 12) {
+                    Console.WriteLine("License Suspended");
+
+
+
+        }
+    }
+}
+    }
+}
+
---
+++ carspeedlimit/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("carspeedlimit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("carspeedlimit")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("97407dcd-7f2b-492f-9161-3d72a1dd7258")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
